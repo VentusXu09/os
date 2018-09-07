386d50f994e7eb473d785d816bfa57aed98c00c4
Xu <ventus.xu@sap.com>
2018-09-07 21:03:34 +0800

    game added

--- command/fiveinarow.c
+++ command/fiveinarow.c
@@ -1,171 +1,313 @@
-#include<stdio.h>
+#include<stdio.h> 
+#include<Windows.h>
 
-char map[10][10] = {
-	'0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
-	'0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
-	'0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
-	'0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
-	'0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
-	'0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
-	'0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
-	'0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
-	'0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
-	'0', '0', '0', '0', '0', '0', '0', '0', '0', '0'
-};
-int judge(int candidate){
-	int i, j,k;
-	char ch = ' ';
-	if (candidate == 0)
-	{
-		ch = 'x';
-	}
-	else
-	{
-		ch = 'v';
-	}
-		for (i = 0; i < 10; i++)
-		{
-			for (j = 0; j < 10; j++)
-			{
-				if (map[i][j] == ch)
-				{
-					if (9 - j >= 4)
-					{
-						for (k = 1; k <= 4; k++)
-						{
-							if (map[i][j + k] != ch)
-							{
-								break;
-							}
-							if (k == 4)
-							{
-								return 1;
-							}
-						}
-					}
-					if (9 - i >= 4)
-					{
-						for (k = 1; k <= 4; k++)
-						{
-							if (map[i + k][j] != ch)
-							{
-								break;
-							}
-							if (k == 4)
-							{
-								return 1;
-							}
-						}
-					}
-					if (9 - j >= 4 && 9 - i >= 4)
-					{
-						for (k = 1; k <= 4; k++)
-						{
-							if (map[i + k][j + k] != ch)
-							{
-								break;
-							}
-							if (k == 4)
-							{
-								return 1;
-							}
-						}
-					}
-					if (i >= 4 && 9-j>=4)
-					{
-						for (k = 1; k <= 4; k++)
-						{
-							if (map[i - k][j + k] != ch)
-							{
-								break;
-							}
-							if (k==4)
-							{
-								return 1;
-							}
-						}
-					}
-				}
-			}
-		}
-		return 0;
+int board[8][8], saveboard[60][8][8];
+int cx, cy, col, pass, empty, black, white;
+
+void init() {  //initialization 
+	memset(board, -1, sizeof(board));
+	board[3][3] = 0;
+	board[3][4] = 1;
+	board[4][4] = 0;
+	board[4][3] = 1;
+	col = 0;
+	pass = 0;
+	empty = 60;
+	black = 2;
+	white = 2;
 }
-int main(void)
-{
-	printf("*************************************************************\n");
-	printf("*                                                           *\n");
-	printf("*              Welcome to five-in-a-row!                    *\n");
-	printf("*         The first hand is the candidate of 'x'.           *\n");
-	printf("*         The second hand is the candidate of 'v'.          *\n");
-	printf("*  You can input coordinate like this 'XX'(double integer). *\n");
-	printf("*   The coordinate is beginning of '00',and end of '99'.    *\n");
-	printf("*            you can end the game by enter '??'               *");
-	printf("*                  Now let's begin!                         *\n");
-	printf("*                                                           *\n");
-	printf("*************************************************************\n");
-	int x, y;
-	int candidate = 0;
-	int win = 0;
-        char coordinate[2]={};
-	for (int i = 0; i < 10; i++)
-		{
-			for (int j = 0; j < 10; j++)
-			{
-				printf("%c ", map[i][j]);
-			}
-			printf("\n");
+
+int input() {
+	char s[1000] = "";
+	scanf("%s", &s);
+	if (s[0] >= 'a' && s[0] <= 'h')
+		cy = s[0] - 'a';
+	else if (s[0] >= 'A' && s[0] <= 'H')
+		cy = s[0] - 'A';
+	else return 0;
+	if (s[1] >= '1' && s[1] <= '8') {
+		cx = s[1] - '1';
+		return 1;
+	}
+	return 0;
+}
+
+int judge(int x, int y) {
+	int i, j, temp;
+	temp = (col + 1) % 2;
+	//left && up 
+	if (board[x - 1][y - 1] == temp) {
+		for (i = x - 1, j = y - 1; i >= 0 && j >= 0; i--, j--) {
+			if (board[i][j]<0) break;
+			if (col == board[i][j]) return 1;
 		}
-	while (1)
-	{
-		if (candidate == 0)
-		{
-			printf("'x'candicate will set:");
+	}
+	//up 
+	if (board[x - 1][y] == temp) {
+		for (i = x - 1; i >= 0; i--) {
+			if (board[i][y]<0) break;
+			if (col == board[i][y]) return 1;
 		}
-		else
-		{
-			printf("'v'candicate will set:");
+	}
+	//right && up 
+	if (board[x - 1][y + 1] == temp) {
+		for (i = x - 1, j = y + 1; i >= 0 && j<8; i--, j++) {
+			if (board[i][j]<0) break;
+			if (col == board[i][j]) return 1;
 		}
-		read(0,coordinate,2);
-		if(coordinate[0]=='?'||coordinate[1]=='?')
-		{
-			printf("game over!");
-			break;
+	}
+	//right 
+	if (board[x][y + 1] == temp) {
+		for (j = y + 1; j<8; j++) {
+			if (board[x][j]<0) break;
+			if (col == board[x][j]) return 1;
 		}
-                x=coordinate[0]-48;
-                y=coordinate[1]-48;
-		if (candidate==0)
-		{
-			map[x][y] = 'x';
-			win = judge(candidate);
-			candidate = 1;
+	}
+	//right && down 
+	if (board[x + 1][y + 1] == temp) {
+		for (i = x + 1, j = y + 1; i<8 && j<8; i++, j++) {
+			if (board[i][j]<0) break;
+			if (col == board[i][j]) return 1;
 		}
-		else
-		{
-			map[x][y] = 'v';
-			win = judge(candidate);
-			candidate = 0;
+	}
+	//down 
+	if (board[x + 1][y] == temp) {
+		for (i = x + 1; i<8; i++) {
+			if (board[i][y]<0) break;
+			if (col == board[i][y]) return 1;
 		}
-		for (int i = 0; i < 10; i++)
-		{
-			for (int j = 0; j < 10; j++)
-			{
-				printf("%c ", map[i][j]);
-			}
-			printf("\n");
+	}
+	//left && down 
+	if (board[x + 1][y - 1] == temp) {
+		for (i = x + 1, j = y - 1; i<8 && j >= 0; i++, j--) {
+			if (board[i][j]<0) break;
+			if (col == board[i][j]) return 1;
 		}
-		if (win == 1)
-		{
-			if (candidate == 1)
-			{
-				printf("'x' win!\n");
+	}
+	//left 
+	if (board[x][y - 1] == temp) {
+		for (j = y - 1; j >= 0; j--) {
+			if (board[x][j]<0) break;
+			if (col == board[x][j]) return 1;
+		}
+	}
+	return 0;
+}
+
+void move(int x, int y) {
+	int i, j, temp, count;
+	temp = (col + 1) % 2;
+	count = 0;
+	//left && up 
+	if (board[x - 1][y - 1] == temp) {
+		for (i = x - 1, j = y - 1; i >= 0 && j >= 0; i--, j--) {
+			if (board[i][j]<0) break;
+			if (col == board[i][j]) {
+				while (i != x) {
+					board[++i][++j] = col;
+					count++;
+				}
+				count--;
 				break;
 			}
+		}
+	}
+	//up 
+	if (board[x - 1][y] == temp) {
+		for (i = x - 1; i >= 0; i--) {
+			if (board[i][y]<0) break;
+			if (col == board[i][y]) {
+				while (i != x) {
+					board[++i][y] = col;
+					count++;
+				}
+				count--;
+				break;
+			}
+		}
+	}
+	//right && up 
+	if (board[x - 1][y + 1] == temp) {
+		for (i = x - 1, j = y + 1; i >= 0 && j<8; i--, j++) {
+			if (board[i][j]<0) break;
+			if (col == board[i][j]) {
+				while (i != x) {
+					board[++i][--j] = col;
+					count++;
+				}
+				count--;
+				break;
+			}
+		}
+	}
+	//right 
+	if (board[x][y + 1] == temp) {
+		for (j = y + 1; j<8; j++) {
+			if (board[x][j]<0) break;
+			if (col == board[x][j]) {
+				while (j != y) {
+					board[x][--j] = col;
+					count++;
+				}
+				count--;
+				break;
+			}
+		}
+	}
+	//right && down 
+	if (board[x + 1][y + 1] == temp) {
+		for (i = x + 1, j = y + 1; i<8 && j<8; i++, j++) {
+			if (board[i][j]<0) break;
+			if (col == board[i][j]) {
+				while (i != x) {
+					board[--i][--j] = col;
+					count++;
+				}
+				count--;
+				break;
+			}
+		}
+	}
+	//down 
+	if (board[x + 1][y] == temp) {
+		for (i = x + 1; i<8; i++) {
+			if (board[i][y]<0) break;
+			if (col == board[i][y]) {
+				while (i != x) {
+					board[--i][y] = col;
+					count++;
+				}
+				count--;
+				break;
+			}
+		}
+	}
+	//left && down 
+	if (board[x + 1][y - 1] == temp) {
+		for (i = x + 1, j = y - 1; i<8 && j >= 0; i++, j--) {
+			if (board[i][j]<0) break;
+			if (col == board[i][j]) {
+				while (i != x) {
+					board[--i][++j] = col;
+					count++;
+				}
+				count--;
+				break;
+			}
+		}
+	}
+	//left 
+	if (board[x][y - 1] == temp) {
+		for (j = y - 1; j >= 0; j--) {
+			if (board[x][j]<0) break;
+			if (col == board[x][j]) {
+				while (j != y) {
+					board[x][++j] = col;
+					count++;
+				}
+				count--;
+				break;
+			}
+		}
+	}
+	board[x][y] = col;
+	if (col) {
+		black += count;
+		white -= count;
+		black++;
+	}
+	else {
+		black -= count;
+		white += count;
+		white++;
+	}
+	empty--;
+}
+
+void output() {
+	system("cls");
+	printf("Input Order like \"C5\" to play the Reversi.\n");
+	char c;
+	printf(" ");
+	for (int i = 0; i<8; i++) {
+		c = 'A' + i;
+		printf("%2c", c);
+	}
+	printf("\n");
+
+	for (int i = 0; i<8; i++) {
+		printf("%d", i + 1);
+		for (int j = 0; j<8; j++) {
+			if (board[i][j] == -1)
+				c = ' ';
+			else if (board[i][j] == 0)
+				c = 'O';
 			else
-			{
-				printf("'v' win!\n");
-				break;
-			}
+				c = 'X';
+			printf("%2c", c);
 		}
+		printf("\n");
+	}
+	printf("Black:%3d     White:%3d\n", black, white);
+}
+
+int passjudge() {
+	int f = 0;
+	for (int i = 0; i<8; i++)
+		for (int j = 0; j<8; j++)
+			if (board[i][j]<0)
+				f += judge(i, j);
+	return f;
+}
+
+void startprint() {
+	printf("1¡¢New game\n0¡¢Exit\n");
+}
+
+void pvp() {
+	while (empty && pass<2) {
+		//black or white 
+		col++;
+		col %= 2;
+		output();
+		//input 
+		if (!input()) {
+			if (!passjudge()) {
+				printf("Pass!\n");
+				pass++;
+			}
+			else {
+				col++;
+				printf("No pass!\nPlease input right stone!\n");
+			}
+			continue;
+		}
+		if (judge(cx, cy)) {
+			move(cx, cy);
+			pass = 0;
+		}
+		else {
+			col++;
+			printf("Miss stone\n");
+		}
+	}
+	output();
+	if (black>white)
+		printf("Black Win!\n");
+	else if (black<white)
+		printf("White Win!\n");
+	else
+		printf("Draw Game!\n");
+	startprint();
+}
+
+int main(int argc, char* argv[]) {
+	int n;
+	startprint();
+	while (scanf("%d", &n) && n) {
+		init();
+		if (n == 1)
+			pvp();
 	}
 	return 0;
 }
--- command/moveBox.c
+++ command/moveBox.c
@@ -1,255 +1,208 @@
 #include<stdio.h>
- 
-#define COL 11
-#define ROW 10
- 
-#define PERSON 'A'
-#define BOX 'X'
-#define WALL '#'
-#define SPACE ' '
-#define ERROR '@'
- 
-#define UP 100
-#define DOWN 101
-#define LEFT 102
-#define RIGHT 103
- 
-char map[ROW][COL] = {
-    "##########",//0  
-    "###     ##",//1  
-    "###     ##",//2  
-    "##AX  # ##",//3  
-    "###  ##  #",//4  
-    "#####    #",//5  
-    "##       #",//6  
-    "#     ####",//7  
-    "###       ",//8  
-    "##########" //9  
-    //A:ÈË  £¬ X£ºÏä×Ó 
-};
-int location[2] = { 3, 2 };
- 
-void printAll()
+#include<stdlib.h>
+#include<time.h>
+#include<conio.h>
+#include<windows.h>
+
+typedef struct{ int x, y; }Point;
+
+char map[22][22];                        //定义一个22*22的地图（含边界）
+Point snake[400], food, Next;            //定义蛇、食物、下一步蛇头的位置
+int head, tail;                            //用于储存蛇头和蛇尾的下标
+int grade, length, autotime;            //游戏等级、蛇长、自动前进所需时间
+char direction;                            //前进方向
+
+//定位光标
+void gotoxy(int x, int y)
 {
-    int i;
-    for (i = 0; i < ROW; i++)
+    HANDLE hConsoleOutput;
+    COORD dwCursorPosition;
+    dwCursorPosition.X = x;
+    dwCursorPosition.Y = y;
+    hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(hConsoleOutput, dwCursorPosition);
+}
+
+//用inline定义内联函数节省程序运行时的调用开销
+//刷新地图
+inline void Update(char map[][22], int grade, int length, int autotime)
+{
+    //system("cls");                        //清屏
+    gotoxy(0, 0);
+    int i, j;
+    printf("\n");
+    for (i = 0; i < 22; i++)
     {
-        printf("%s\n", map[i]);
+        printf("\t");
+        for (j = 0; j < 22; j++)
+            printf("%c ", map[i][j]);
+        if (i == 0)
+            printf("\t等级为：%d", grade);
+        if (i == 2)
+            printf("\t长度为：%d", length);
+        if (i == 6)
+            printf("\t自动前进时间");
+        if (i == 8)
+            printf("\t间隔为：%d ms", autotime);
+        printf("\n");
     }
 }
-int win()
+
+//欢迎界面
+inline void hello()
 {
-    if (map[8][9]=='X')
+    puts("\n\n\n\t\t\t贪吃蛇游戏即将开始！");                        //准备开始
+    double start;
+    for (int i = 3; i >= 0; i--)
     {
-        return 1;
+        start = (double)clock() / CLOCKS_PER_SEC;                //得到程序目前为止运行的时间
+        while ((double)clock() / CLOCKS_PER_SEC - start <= 1);    //经过1秒之后
+        if (i > 0)
+        {
+            system("cls");                                        //清屏
+            printf("\n\n\n\t\t\t进入倒计时：%d\n", i);                //倒计时
+        }
+        else
+            Update(map, grade, length, autotime);                //刷新地图
     }
-    return 0;
 }
-char boxNear(int direction)
+
+//随机生成食物位置
+inline void f()
 {
+    srand(int(time(0)));                                        //调用种子函数
+    do{
+        food.x = rand() % 20 + 1;
+        food.y = rand() % 20 + 1;
+    } while (map[food.x][food.y] != ' ');
+    map[food.x][food.y] = '!';                                    //食物为“!”
+}
+
+//初始化
+inline void init()
+{
+    int i, j;
+    for (i = 1; i <= 20; i++)
+        for (j = 1; j <= 20; j++)
+            map[i][j] = ' ';
+    for (i = 0; i <= 21; i++)
+        map[0][i] = map[21][i] = map[i][0] = map[i][21] = '*';    //边界
+    map[1][1] = map[1][2] = 'O';                                //蛇身（含蛇尾）
+    map[1][3] = '@';                                            //蛇头
+    head = 2; tail = 0;                                            //开始时头和尾的下标
+    snake[head].x = 1; snake[head].y = 3;                        //开始时蛇头在地图上的下标
+    snake[tail].x = 1; snake[tail].y = 1;                        //开始时蛇尾在地图上的下标
+    snake[1].x = 1; snake[1].y = 2;                                //开始时蛇身在地图上的下标
+    f();                                                        //随机生成食物位置
+    grade = 1; length = 3; autotime = 500;                        //开始的等级、长度、自动前进时间
+    direction = 77;                                                //初始的运动方向向右
+}
+
+//预前进
+inline int GO()
+{
+    bool timeover = true;
+    double start = (double)clock() / CLOCKS_PER_SEC;            //得到程序目前为止运行的时间
+
+L:
+    //自动经过1秒或者等待1秒内的键盘输入
+    while ((timeover = ((double)clock() / CLOCKS_PER_SEC - start <= autotime / 1000.0)) && !_kbhit());
+    //键盘输入
+    if (timeover)
+    {
+        //_getch();
+        char d = _getch();                                    //获取方向
+        if (d != 72 && d != 80 && d != 75 && d != 77 || direction == 72 && d == 80 || direction == 80 && d == 72 || direction == 75 && d == 77 || direction == 77 && d == 75)
+            goto L;
+        else
+            direction = d;
+    }
     switch (direction)
     {
-    case UP:
-        if (map[location[0] - 2][location[1]]==WALL)
-        {
-            return WALL;
-        }
-        else
-        {
-            return SPACE;
-        }
-    case DOWN:
-        if (map[location[0] + 2][location[1]] == WALL)
-        {
-            return WALL;
-        }
-        else
-        {
-            return SPACE;
-        }
-    case LEFT:
-        if (map[location[0]][location[1]-2] == WALL)
-        {
-            return WALL;
-        }
-        else
-        {
-            return SPACE;
-        }
-    case RIGHT:
-        if (map[location[0]][location[1]+2] == WALL)
-        {
-            return WALL;
-        }
-        else
-        {
-            return SPACE;
-        }
-    default:
-        return WALL;
+    case 72:
+        Next.x = snake[head].x - 1; Next.y = snake[head].y;        //向上
         break;
+    case 80:
+        Next.x = snake[head].x + 1; Next.y = snake[head].y;        //向下
+        break;
+    case 75:
+        Next.x = snake[head].x; Next.y = snake[head].y - 1;        //向左
+        break;
+    case 77:
+        Next.x = snake[head].x; Next.y = snake[head].y + 1;        //向右
+        break;
+    default:
+        puts("\tGame over!");                                    //按下非方向键游戏失败
+        return 0;
     }
+    if (Next.x == 0 || Next.x == 21 || Next.y == 0 || Next.y == 21)            //撞到边界
+    {
+        puts("\tGame over!");
+        return 0;
+    }
+    if (map[Next.x][Next.y] != ' '&&!(Next.x == food.x&&Next.y == food.y))    //吃到自己
+    {
+        puts("\tGame over!");
+        return 0;
+    }
+    if (length == 400)                                                        //最长蛇长
+    {
+        puts("\tGood game!");
+        return 0;
+    }
+    return 1;
 }
-void moveUP()
+
+//吃到食物
+inline void EAT()
 {
-    switch (map[location[0] -1][location[1]])
+    length++;                                        //长度增加1
+    int _grade = length / 10 + 1;                    //计算等级
+    if (_grade != grade)
     {
-    case WALL:
-        break;
-    case BOX:
-        if (boxNear(UP)!=WALL)
-        {
-            map[location[0]][location[1]] = ' ';
-            map[location[0] - 1][location[1]] = 'A';
-            map[location[0] - 2][location[1]] = 'X';
-            location[0] -= 1;
-        }
-        break;
-    case SPACE:
-        map[location[0]][location[1]] = ' ';
-        map[location[0] - 1][location[1]] = 'A';
-        location[0] -= 1;
-        break;
-    default:
-        printf("UP error\n");
-        return;
-        break;
+        grade = _grade;
+        if (autotime >= 100)
+            autotime = 550 - grade * 50;            //增加一级自动时间减短50毫秒
     }
+    map[Next.x][Next.y] = '@';                        //蛇头位置变化
+    map[snake[head].x][snake[head].y] = 'O';        //原蛇头位置变化为蛇身
+    head = (head + 1) % 400;                        //蛇头下标加1
+    snake[head].x = Next.x; snake[head].y = Next.y;    //蛇头下标变化
+    f();                                            //随机生成食物位置
+    Update(map, grade, length, autotime);            //刷新地图
 }
-void moveDOWN()
+
+//没吃到食物
+inline void FAILURE()
 {
-    switch (map[location[0] +1][location[1]])
-    {
-    case WALL:
-        break;
-    case BOX:
-        if (boxNear(DOWN) != WALL)
-        {
-            map[location[0]][location[1]] = ' ';
-            map[location[0] + 1][location[1]] = 'A';
-            map[location[0] + 2][location[1]] = 'X';
-            location[0] += 1;
-        }
-        break;
-    case SPACE:
-        map[location[0]][location[1]] = ' ';
-        map[location[0] + 1][location[1]] = 'A';
-        location[0] += 1;
-        break;
-    default:
-        printf("DOWN error\n");
-        return;
-        break;
-    }
+    map[snake[tail].x][snake[tail].y] = ' ';        //蛇尾原来的位置变成“ ”
+    tail = (tail + 1) % 400;                        //蛇尾下标加1
+    map[Next.x][Next.y] = '@';                        //蛇头位置变化
+    map[snake[head].x][snake[head].y] = 'O';        //原蛇头位置变化为蛇身
+    head = (head + 1) % 400;                        //蛇头下标加1
+    snake[head].x = Next.x;                            //蛇头下标变化
+    snake[head].y = Next.y;
+    Update(map, grade, length, autotime);            //刷新地图
 }
-void moveLEFT(){
-    switch (map[location[0]][location[1]-1])
-    {
-    case WALL:
-        break;
-    case BOX:
-        if (boxNear(LEFT) != WALL)
-        {
-            map[location[0]][location[1]] = ' ';
-            map[location[0]][location[1]-1] = 'A';
-            map[location[0]][location[1]-2] = 'X';
-            location[1] -= 1;
-        }
-        break;
-    case SPACE:
-        map[location[0]][location[1]] = ' ';
-        map[location[0]][location[1]-1] = 'A';
-        location[1] -= 1;
-        break;
-    default:
-        printf("LEFT error\n");
-        return;
-        break;
-    }
-}
-void moveRIGHT(){
-    switch (map[location[0]][location[1]+1])
-    {
-    case WALL:
-        break;
-    case BOX:
-        if (boxNear(RIGHT) != WALL)
-        {
-            map[location[0]][location[1]] = ' ';
-            map[location[0]][location[1]+1] = 'A';
-            map[location[0]][location[1]+2] = 'X';
-            location[1] += 1;
-        }
-        break;
-    case SPACE:
-        map[location[0]][location[1]] = ' ';
-        map[location[0]][location[1]+1] = 'A';
-        location[1] += 1;
-        break;
-    default:
-        printf("RIGHT error\n");
-        return;
-        break;
-    }
-}
-void run(){
-    char direction = ' ';
-    char dir[1]={};
+
+//main函数
+int main()
+{
+    system("color F0");
+    init();                    //初始化
+    hello();                //欢迎界面
     while (1)
     {
-        read(0,dir,1);
-	direction=dir[0];
-        if (direction == 'w' || direction == 'W')
+        if (GO())            //预前进
         {
-            moveUP();
-        }
-        else if (direction == 's' || direction == 'S')
-        {
-            moveDOWN();
- 
-        }
-        else if (direction == 'a' || direction == 'A')
-        {
-            moveLEFT();
-        }
-        else if (direction == 'd' || direction == 'D')
-        {
-            moveRIGHT();
-        }
-        else if (direction=='Q'||direction=='q')
-        {
-            printf("game over!\n");
-            break;
+            if (Next.x == food.x&&Next.y == food.y)
+                EAT();        //吃到食物
+            else
+                FAILURE();    //没吃到食物
         }
         else
-        {
-            printf("No such direction\n");
-        }
-        printAll();
-        if (win())
-        {
-            printf("you win!\n");
-            break;
-        }
+            return 0;        //失败或者胜利，游戏结束
     }
-}
-void init()
-{
-    printf("******************************************\n");
-    printf("*                                        *\n");
-    printf("*          welcome to move box!!         *\n");
-    printf("*              'A' is human              *\n");
-    printf("*               'X' is box               *\n");
-    printf("*           enter w s a d to move        *\n");
-    printf("*            enter q to quit             *\n");
-    printf("*  if you move the box the lower right,  *\n");
-    printf("*               you will win!            *\n");
-    printf("*                                        *\n");
-    printf("******************************************\n");
-}
-int main(){
-    init();
-    printAll();
-    run();
     return 0;
 }
+//
